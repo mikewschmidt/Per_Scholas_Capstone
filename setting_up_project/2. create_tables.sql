f0a060049fa5f0d3560b384681aa2680480068d1
mikewschmidt <michaelwschmidt@gmail.com>
2023-06-07 23:53:46 -0500

    changed the loan table dependants column to varchar

--- setting_up_project/2. create_tables.sql
+++ setting_up_project/2. create_tables.sql
@@ -85,7 +85,7 @@ CREATE TABLE IF NOT EXISTS cdw_sapp_loan_application (
 	Application_ID varchar(10),
 	Gender varchar(10),
 	Married char(3),
-	Dependents int,
+	Dependents varchar(3),
 	Education varchar(15),
 	Self_Employed varchar(3),
 	Credit_History int,
